4a6614e1c862f72f64178a3264cd88225ea9406b
DAW <DAW@DESKTOP-U4FTC0L>
2023-05-05 13:59:57 +0200

    Segundo comit

--- Projecto1/src/Paquete1/Principal.java
+++ Projecto1/src/Paquete1/Principal.java
@@ -2,8 +2,7 @@ package Paquete1;
 
 public class Principal {
 	public static void main(String args[]) {
-		
-	
 	System.out.println("Control de versiones con Git");
+	System.out.println("Control de versiones con Git2");
 	}
 }
